56857553137b93d2f9990cb6780a05595ddd461d
Daniel <daniel.j.groch@gmail.com>
2021-03-28 19:23:11 +1100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  siteMetadata: {
+    title: "My Gatsby Site",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        url: "https://www.testing.figandbloom.com.au/graphql",
+      },
+    },
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: " UA-59881247-1",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
